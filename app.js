042063a70087623b2f4131ba552125a7f412a383
Sree1710 <kunjusree817@gmail.com>
2022-07-22 20:43:49 +0530

    backend completed

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ friendList=[
         "DescribeYourFriend":"wise"
     }
 ];
+app.post('/add',function(req,res){
+console.log(req.body);
+friendList.push(req.body);
+res.status(200).send(friendList);
+});
 app.get('/view',function(req,res){
 res.send(friendList);
 });
